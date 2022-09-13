9b51c3b4211ea567d849ed54dbc5ce53a90cf836
Pamberg Vyacheslav <31064596+VyacheslavPamberg@users.noreply.github.com>
2022-09-14 01:50:25 +0500

    Update PrivateData.java

--- src/main/java/org/selenide/exaples/constants/PrivateData.java
+++ src/main/java/org/selenide/exaples/constants/PrivateData.java
@@ -1,6 +1,6 @@
 package org.selenide.exaples.constants;
 
 public class PrivateData {
-    public static final String PHONE_NUMBER_FOR_AUTHORIZATION = "+16466333353";
-    public static final String PASSWORD_FOR_AUTHORIZATION = "rt3dD2#l21,bt@";
+    public static final String PHONE_NUMBER_FOR_AUTHORIZATION = "";
+    public static final String PASSWORD_FOR_AUTHORIZATION = "";
 }
